1d58ce96b3afb7def91097d65cfcda377c54e84f
subraban <136383571+subraban@users.noreply.github.com>
2023-07-11 01:17:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ resource "null_resource" "import_backup" {
     command = <<EOT
       gcloud auth activate-service-account --key-file=creds.json
       echo Y | gcloud sql import sql instance1 gs://backupandrestoredborasql/DB1.bak --database=db1 --quiet
-     gcloud sql import sql instance1 gs://backupandrestoredborasql/DB2.bak --database=db2 --quiet
-     gcloud sql import sql instance1 gs://backupandrestoredborasql/DB3.bak --database=db3 --quiet  
+     
     EOT
 }
 }
