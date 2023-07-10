c445747abf0596f9e1183d1974a0ff988bd056ca
subraban <136383571+subraban@users.noreply.github.com>
2023-07-11 01:11:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ resource "null_resource" "import_backup" {
   provisioner "local-exec" {
     command = <<EOT
       gcloud auth activate-service-account --key-file=creds.json
-      echo Y | gcloud sql import sql instance3 gs://sqlservermedia/WideWorldImporters-Full.bak.bak --database=db1 --quiet
-     
+      echo Y | gcloud sql import sql instance1 gs://backupandrestoredborasql/DB1.bak --database=db1 --quiet
+      echo Y | gcloud sql import sql instance1 gs://backupandrestoredborasql/DB2.bak --database=db2 --quiet
+      echo Y | gcloud sql import sql instance1 gs://backupandrestoredborasql/DB3.bak --database=db3 --quiet  
     EOT
 }
 }
