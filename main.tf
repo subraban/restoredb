58c9b3a4e4fca4718e31842947c363da80387fbd
subraban <136383571+subraban@users.noreply.github.com>
2023-07-11 01:32:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "null_resource" "import_backup" {
     EOT
 }
 }
+service_account {
+    email  = "101674418114-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
